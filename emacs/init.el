70fd31367c1b87e0416964dbe80d5b462d8f3a21
Rafiyq Widianto <rafiyqw@tutanota.com>
2020-03-26 18:45:31 +0700

    Disable optimization setting

--- emacs/init.el
+++ emacs/init.el
@@ -1,20 +1,20 @@
 ;; init.el --- Emacs configuration file -*- lexical-binding: t -*-
 
-;; Optimization
-;;; Garbage collection & File name handler
-(defvar default-file-name-handler-alist file-name-handler-alist)
-(setq gc-cons-threshold most-positive-fixnum
-      gc-cons-percentage 0.6
-      file-name-handler-alist nil)
+;; ;; Optimization
+;; ;;; Garbage collection & File name handler
+;; (defvar default-file-name-handler-alist file-name-handler-alist)
+;; (setq gc-cons-threshold most-positive-fixnum
+;;       gc-cons-percentage 0.6
+;;       file-name-handler-alist nil)
 
-(add-hook 'emacs-startup-hook
-  (lambda ()
-    (setq gc-cons-threshold 16777216
-          gc-cons-percentage 0.1
-          file-name-handler-alist default-file-name-handler-alist)))
+;; (add-hook 'emacs-startup-hook
+;;   (lambda ()
+;;     (setq gc-cons-threshold 16777216
+;;           gc-cons-percentage 0.1
+;;           file-name-handler-alist default-file-name-handler-alist)))
 
-;;; Inhibit resize frame
-(setq frame-inhibit-implied-resize t)
+;; ;;; Inhibit resize frame
+;; (setq frame-inhibit-implied-resize t)
 
 
 
